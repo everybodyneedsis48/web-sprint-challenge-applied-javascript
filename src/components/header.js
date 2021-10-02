23fa32b353c59936497231059e3f399d7644ee72
everybodyneedsis <jacobkylefranklin@gmail.com>
2021-10-02 13:45:29 -0400

    finished building out header component, all tests working

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,24 @@
 const Header = (title, date, temp) => {
+
+  const header = document.createElement('div');
+  const headerDate = document.createElement('span');
+  const headerTitle = document.createElement('h1');
+  const headerTemp = document.createElement('span');
+
+  headerDate.textContent = date
+  headerTitle.textContent = title
+  headerTemp.textContent = temp
+
+  header.classList.add('header');
+  headerDate.classList.add('date');
+  headerTemp.classList.add('temp');
+
+  header.appendChild(headerDate);
+  header.appendChild(headerTitle);
+  header.appendChild(headerTemp);
+
+  return header;
+
   // TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
@@ -14,6 +34,10 @@ const Header = (title, date, temp) => {
 }
 
 const headerAppender = (selector) => {
+
+  const header = Header()
+
+  document.querySelector(selector).appendChild(header)
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
